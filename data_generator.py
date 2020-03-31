f7d89022981832331528621c0977a49b277a391b
Aarya Patel <aaryapatel98@gmail.com>
2020-04-01 02:22:15 +0530

    Update data_generator.py
    Add plot_confusion_metric function to draw the confusion matrix for inference.

--- data_generator.py
+++ data_generator.py
@@ -3,6 +3,8 @@ import numpy as np
 import cv2
 import glob
 import gc
+import itertools
+import matplotlib.pyplot as plt
 from albumentations import (Resize,Compose, RandomRotate90, Transpose, Flip, OneOf, CLAHE, IAASharpen, IAAEmboss,
 RandomBrightnessContrast, JpegCompression, Blur, GaussNoise, HueSaturationValue, ShiftScaleRotate, Normalize)
 from random import shuffle
@@ -36,7 +38,38 @@ def do_train_augmentations():
 def do_inference_aug():
     return Compose([Normalize(p=1)], p=1)
 
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
 
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+    plt.savefig('octresnet_cm.png')
+  
 def data_gen(list_files, id_label_map_in, batch_size_in, img_size_in, aug_funtion):
   
     aug = aug_funtion()
